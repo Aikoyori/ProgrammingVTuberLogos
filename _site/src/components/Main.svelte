1641c8d3d6fe5a4bd589325c83dc5739f2c0df3a
Manassarn Manoonchai <manassarn@gmail.com>
2024-04-22 21:20:58 +0700

    Search

--- _site/src/components/Main.svelte
+++ _site/src/components/Main.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
   import { onMount } from "svelte"
 
+  import Fuse from "fuse.js"
+
   interface ImageFile {
     src: string
     folder: string
@@ -9,6 +11,9 @@
   }
 
   let images: ImageFile[] = []
+  let filteredImages: ImageFile[] = []
+  let search: string = ""
+  let fuse: Fuse<ImageFile>
 
   onMount(async () => {
     const files = await import.meta.glob<{
@@ -27,26 +32,58 @@
         }
       })
       .filter((image) => !image.name.includes("Old.")) // Filter out images with "Old." in the name
+
+    fuse = new Fuse(images, {
+      keys: ["folder", "name"],
+    })
   })
+
+  $: filteredImages =
+    fuse && search.length
+      ? fuse.search(search)?.map((result) => result.item) ?? []
+      : images
 </script>
 
 <main>
+  <h1>Programming VTuber Logos</h1>
+
+  <p>
+    <input type="text" placeholder="Search..." bind:value={search} />
+  </p>
+
   <table border="1">
     <thead>
       <tr>
-        <th>Path</th>
+        <th>File</th>
         <th>Image</th>
       </tr>
     </thead>
     <tbody>
-      {#each images as { src, folder, name, format }, i}
+      {#if !filteredImages.length}
         <tr>
-          <td>{folder}/{name}</td>
+          <td colspan="2">No results found</td>
+        </tr>
+      {/if}
+      {#each filteredImages as { src, folder, name, format }, i}
+        <tr>
+          <td
+            ><a
+              href={`https://github.com/Aikoyori/ProgrammingVTuberLogos/blob/main/${folder}/${name}`}
+              target="_blank">{folder}/{name}</a
+            ></td
+          >
           <td><img {src} alt={name} style="width: 300px;" /></td>
         </tr>
       {/each}
     </tbody>
   </table>
+
+  <p>
+    Can't find what you're looking for? <a
+      href="https://github.com/Aikoyori/ProgrammingVTuberLogos"
+      target="_blank">Request a new logo</a
+    >
+  </p>
 </main>
 
 <style>
@@ -62,5 +99,9 @@
 
   td {
     padding: 2rem 1rem;
+  }
+
+  a {
+    color: white;
   }
 </style>
